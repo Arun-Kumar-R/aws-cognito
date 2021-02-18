e5fb400ed7a99891127103bd60b7b3fa9db768c1
Arun-Kumar-R <rarunkumar0298@gmail.com>
2021-02-18 18:03:50 +0530

    backend base code setup

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const path = require('path');
+const Mongoose = require('mongoose');
+
+//Custom files
+const env = require('./config/env');
+
+//express app initialization
+const app = express();
+
+// @Database connection
+Mongoose.connect(env.dbCon, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const DB = Mongoose.connection;
+
+// @DB Error
+DB.on('error', () => {
+    console.log('>Error occurred from the Database');
+});
+
+// @DB success
+DB.once('open', () => {
+    console.log(">Database connected succesfully");
+});
+
+// @middleware body parser
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// @log the output request
+app.use(morgan('dev'));
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(__dirname, 'client', 'build', 'index.html');
+    });
+}
+
+// @setup Port
+const PORT = env.apiPort;
+
+// @listen port
+app.listen(PORT, () => {
+    console.log('Magic Happens on Port: ' + PORT);
+});
+
+
